7e85a6f5150cf6690d1336abc522d2b00c922d8d
terjolskiterjolski <162707331+terjolskiterjolski@users.noreply.github.com>
2025-10-22 11:46:34 +0400

    mesij

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import pygame
-print("Hello World")
-print("hi")
-print("Hello World")
+import sys
+import math
+ROWS = 10
+COLS = 15
+
+TILESIZE = 32
+WINDOWWIDTH = TILESIZE * COLS
+WINDOWHEIGHT = TILESIZE * ROWS
+
+FOV = 60 *(math.pi/180)
+RESOLUTION = 4
+NUMBEROFRAYS = WINDOWWIDTH/RESOLUTION
+SCREEN = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+while(True):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    SCREEN.fill((255,0,0))
+    pygame.display.update()
